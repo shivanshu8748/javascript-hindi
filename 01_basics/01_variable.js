515eb9045059228425ad268719156b4c68e430df
Shivanshu kumar <shivanshu.ku2000@gmail.com>
2024-11-18 14:09:29 +0000

    variable

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+ const accountId = 144553 // we cant change the value
+ let accountEmail = "shivanshu@google.com"
+ var accountPassword = "12345"
+ accountCity ="jaipur"
+
+ //accountId = 2
+
+ console.log(accountId);
+ 
