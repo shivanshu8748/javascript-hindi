608abd66beb4ac863145ade6ca35f8f5343bbd76
Shivanshu kumar <shivanshu.ku2000@gmail.com>
2024-11-19 13:23:39 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof (score));  
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber);
+
+ 
+
+let isLoggedIn ="shivanshu"
+
+let booleanIsLoggedIn =Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+let someNumber =33
+
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber);
