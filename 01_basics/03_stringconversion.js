692052bc3156de45ae808284cf9fe97eeb57c102
Shivanshu kumar <shivanshu.ku2000@gmail.com>
2024-11-22 08:14:54 +0000

    conversiono of string

--- 01_basics/03_stringconversion.js
+++ 01_basics/03_stringconversion.js
@@ -16,3 +16,8 @@ let str2 = "shivanshu"
 let str3 = str1 = str2
 console.log(str3)
 
+console.log("1"+ 2);     
+console.log(1 + "2");
+console.log("1" + "2");
+console.log("1" +2 +2);
+console.log(1 + 2 "2");
