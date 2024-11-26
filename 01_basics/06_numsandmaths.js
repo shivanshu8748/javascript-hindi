e5a0443c26a13c5b63fecbdc7416cc7827374582
Shivanshu kumar <shivanshu.ku2000@gmail.com>
2024-11-26 10:17:30 +0000

    number&maths

---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,15 @@
+const score = 400
+console.log(score);
+
+const balance = New Number(100)
+console.loh(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber =123.8966
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString);
