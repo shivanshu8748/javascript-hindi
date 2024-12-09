5c224bf34bbe7c8f7541f49bb73655bf3ebba569
Shivanshu kumar <shivanshu.ku2000@gmail.com>
2024-12-09 13:48:25 +0000

    obj

---
+++ 02_basics/03_object.js
@@ -0,0 +1,14 @@
+// object literals
+const jsUser ={
+name: "shivanshu",
+mySym: "mykey1",
+age :18,
+location: "Gaya",
+email: "shivanshu@google.com",
+isLoggedIn:false
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"])
+console.log(jsUser["full name"])
+console.log(typeof jsUser.mySym)
